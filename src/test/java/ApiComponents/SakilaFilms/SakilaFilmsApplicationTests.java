3c27d2252ac53fb7ddc0155937b1e00c8de577ba
Robert <acing-psn@outlook.com>
2022-11-03 10:31:14 +0000

    Added mockito tests

--- src/test/java/ApiComponents/SakilaFilms/SakilaFilmsApplicationTests.java
+++ src/test/java/ApiComponents/SakilaFilms/SakilaFilmsApplicationTests.java
@@ -3,58 +3,128 @@ package ApiComponents.SakilaFilms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 class SakilaFilmsApplicationTests {
-
-/*
-    ActorRepo actorRepo;
-    FilmRepo filmRepo;
-
-    SakilaFilmsApplication testSakila = new SakilaFilmsApplication(actorRepo,filmRepo);
 
     @Test
     void testGetAllFilms()
     {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
         testSakila.getAllFilms();
+
+        verify(testSakila).getAllFilms();
     }
 
     @Test
     void testGetFilmId()
     {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
         testSakila.getFilmId(1);
+
+        verify(testSakila).getFilmId(1);
+    }
+
+    @Test
+    void testGetFilmTitle()
+    {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        testSakila.getFilmTitle("ENCINO ELF");
+
+        verify(testSakila).getFilmTitle("ENCINO ELF");
+    }
+
+    @Test
+    void testGetFilmRating()
+    {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        testSakila.getFilmRating("PG");
+
+        verify(testSakila).getFilmRating("PG");
+    }
+
+    @Test
+    void testGetFilmLength()
+    {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        testSakila.getFilmLength(101);
+
+        verify(testSakila).getFilmLength(101);
     }
 
     @Test
     void testGetAllActors()
     {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
         testSakila.getAllActors();
+
+        verify(testSakila).getAllActors();
     }
 
     @Test
-    void testGetActor()
+    void testGetActorId()
     {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
         testSakila.getActorId(1);
-    }
 
-    @Test
-    void testPostActor()
-    {
-        testSakila.save();
+        verify(testSakila).getActorId(1);
     }
 
     @Test
     void testUpdateActor()
     {
-        testSakila.updateActor();
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        Actor testActor = new Actor(1,"First","Last",new ArrayList<>());
+
+        testSakila.updateActor(1,testActor);
+
+        verify(testSakila).updateActor(1,testActor);
+    }
+
+    @Test
+    void testSave()
+    {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        Actor testActor = new Actor(1,"First","Last",new ArrayList<>());
+
+        testSakila.save(testActor);
+
+        verify(testSakila).save(testActor);
     }
 
     @Test
     void testDeleteActor()
     {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
         testSakila.deleteActor(1);
+
+        verify(testSakila).deleteActor(1);
     }
 
- */
+    @Test
+    void testGet()
+    {
+        SakilaFilmsApplication testSakila = mock(SakilaFilmsApplication.class);
+
+        testSakila.get();
+
+        verify(testSakila).get();
+    }
+
 
 
 	/*
